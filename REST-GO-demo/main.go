33c83ba5a8b79ba9ea77312a8fcb08095eeb266e
wangqianchuang <wangqianchuang@smartisan.com>
2018-05-31 23:01:12 +0800

    update main.go
    Signed-off-by: wangqianchuang <wangqianchuang@smartisan.com>

--- REST-GO-demo/main.go
+++ REST-GO-demo/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/MsloveDl/HuobiProAPI/services"
 )
 
 func main() {
 	fmt.Println("火币")
+	acountData := services.GetAccounts().Data
+	if len(acountData) > 0 {
+		id := acountData[0].ID
+		fmt.Println("%d", id)
+	} else {
+		fmt.Println("no valid data!!")
+	}
 }
